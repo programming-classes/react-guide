2c1a44d5c253f2fe16853e5a55fd8b62cdc3cd21
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2021-05-21 18:20:17 +0100

    use an arrow function

--- src/CharComponent/CharComponent.js
+++ src/CharComponent/CharComponent.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./CharComponent.css";
 
-function getRandomColor() {
+const getRandomColor = () => {
   var letters = "0123456789ABCDEF";
   var color = "#";
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
+};
 
 const CharComponent = (props) => {
   const color = getRandomColor();
