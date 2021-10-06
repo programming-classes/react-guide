a3e8be59ce78e82b59b20432b76c10fa4ae6d137
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2021-10-06 18:16:31 +0100

    refactor new task item form to use form and add elements

--- src/components/NewTaskItemForm.js
+++ src/components/NewTaskItemForm.js
@@ -1,20 +1,50 @@
+import { useState } from "react/cjs/react.development";
 import "./NewTaskItemForm.css";
 
 const NewTaskItemForm = (props) => {
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  const [newTaskDueDate, setNewTaskDueDate] = useState("");
+
+  // Implementar los handlers de la fecha y el owner
+  const newTaskTitleChangeHandler = (event) => {
+    setNewTaskTitle(event.target.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    props.newTaskItemHandler({ title: newTaskTitle });
+  };
+
   return (
-    <div className="input-component">
+    <form className="input-component" onSubmit={onSubmitHandler}>
+      <div>
+        {newTaskTitle.length == 0 ? null : (
+          <label htmlFor="newTaskTitleInput">New task title</label>
+        )}
+        <input
+          onChange={newTaskTitleChangeHandler}
+          className="input-component__input"
+          type="text"
+          id="newTaskTitleInput"
+          placeholder="New task title"
+        />
+      </div>
+      <div>
+        <label htmlFor="newTaskDueDateInput">New task due date</label>
+        <input type="date" id="newTaskDueDateInput" />
+      </div>
+      <select name="ownerName">
+        {props.owners.map((owner) => (
+          <option value={owner}>{owner}</option>
+        ))}
+      </select>
       <input
-        onChange={props.taskTitleChangeHandler}
-        className="input-component__input"
-        type="text"
-      />
-      <button
-        onClick={props.newTaskItemHandler}
+        type="submit"
         className="input-component__button"
-      >
-        Add
-      </button>
-    </div>
+        value="whatever"
+      />
+    </form>
   );
 };
 
