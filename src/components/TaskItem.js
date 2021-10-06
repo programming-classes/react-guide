ecaba4ac95c591574a487e14c2f0b529872b6629
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2021-10-06 18:14:50 +0100

    add owner and date to task item

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -4,9 +4,21 @@ const TaskItem = (props) => {
   const task = props.task;
   return (
     <div className={`task-item ${task.checked && "checked"}`}>
-      <input onChange={props.checkHandler} className="task-item__checked" checked={task.checked} type="checkbox"></input>
-      <h3 className="task-item__title">{task.title}</h3>
-      <span onClick={props.deleteHandler} className="task-item__trash dashicons dashicons-trash" />
+      <input
+        onChange={props.checkHandler}
+        className="task-item__checked"
+        checked={task.checked}
+        type="checkbox"
+      ></input>
+      <div>
+        <h3 className="task-item__title">{task.title}</h3>
+        <p className="task-item__owner">{task.owner}</p>
+        <p className="task-item__dueDate">{task.dueDate}</p>
+      </div>
+      <span
+        onClick={props.deleteHandler}
+        className="task-item__trash dashicons dashicons-trash"
+      />
     </div>
   );
 };
